2be6c733d1b289fe16062a2ea79437ef4ca5d6dd
orehalexeev <alexey.orehoff@gmail.com>
2023-12-14 22:57:05 +0300

    Added report3.docx
    Minor changes

--- lab3/lab3_4.cpp
+++ lab3/lab3_4.cpp
@@ -1,4 +1,8 @@
-#include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <vector>
 
 std::vector<std::vector<cv::Point>> find_contours(cv::Mat image) {
     std::vector<std::vector<cv::Point>> contours;
